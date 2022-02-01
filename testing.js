12131aa74cfacb2cb941df81a642469436d7aa5b
Zarif Karim <zarifkarim09@gmail.com>
2022-02-02 02:04:34 +1100

    update testing.js

--- testing.js
+++ testing.js
@@ -6,12 +6,14 @@ const { throws } = require("assert");
 /* Printing colourful lines in console:
     console.log(color, 'this wiil be colored');
 */
-const color_red = '\x1b[31m%s\x1b[0m';
-const color_green = '\x1b[32m%s\x1b[0m';
-const color_yellow = '\x1b[33m%s\x1b[0m';
-const color_blue = '\x1b[34m%s\x1b[0m';
-const color_magenta = '\x1b[35m%s\x1b[0m';
-const color_cyan = '\x1b[36m%s\x1b[0m';
+const color = {
+    red : '\x1b[31m%s\x1b[0m',
+    green : '\x1b[32m%s\x1b[0m',
+    yellow : '\x1b[33m%s\x1b[0m',
+    blue : '\x1b[34m%s\x1b[0m',
+    magenta : '\x1b[35m%s\x1b[0m',
+    cyan : '\x1b[36m%s\x1b[0m'
+}
 
 
 
@@ -19,102 +21,99 @@ const color_cyan = '\x1b[36m%s\x1b[0m';
 class Testing {
 
     constructor(){
-        this.TESTS = [];
+        //this.TESTS = [];
+        this.total_tests = 0;
         this.passed_tests = 0;
         this.failed_tests = 0;
     }
     
     passed_test(name) {
         this.passed_tests += 1;
-        console.log(color_green, `Passed: ${name}`);
+        console.log(color.green, `Passed: ${name}`);
     }
     
-    failed_test(test_obj,actual) {
+    failed_test(name,actual,expected) {
         this.failed_tests += 1;
-        console.log(color_red, `Failed: ${test_obj.name}`,{
-            Expected : test_obj.expected,
+        console.log(color.red, `Failed: ${name}`,{
+            Expected : expected,
             Returned : actual
         });
-        //console.log(color_green, `\tExpected: ${test_obj.expected}`)
-        //console.log(color_red, `\tReturned: ${actual}`)
     }
     
-    summary_tests() {
-        console.log(color_magenta, '\n------------------------------\n');
-        console.log(color_cyan, `${this.TESTS.length} total Test Cases`);
-        console.log(color_green, `${this.passed_tests} Test Cases Passed`);
-        console.log(color_red, `${this.failed_tests} Test Cases Failed`);
-        console.log(color_magenta, '\n------------------------------\n');
+    summary() {
+        console.log(color.magenta, '\n------------------------------\n');
+        console.log(color.cyan, `${this.total_tests} total Test Cases`);
+        console.log(color.green, `${this.passed_tests} Test Cases Passed`);
+        console.log(color.red, `${this.failed_tests} Test Cases Failed`);
+        console.log(color.magenta, '\n------------------------------\n');
     }
     
-    reset_stats() {
-        this.passed_tests = 0;
-        this.failed_tests = 0;
-    }
+    // reset_stats() {
+    //     this.total_tests = 0;
+    //     this.passed_tests = 0;
+    //     this.failed_tests = 0;
+    // }
 
     //input : function reference : the function to test
     //expected_output : the exact output expected for the function (type needs to match)
-    add(descripton,input_function,expected_output, /*options = {is_async : false}*/){
-        const newTest = {
-            name : descripton,
-            execute : input_function,
-            expected : expected_output
-            //options : options
-        }
+    // add(descripton,actual_output,expected_output, /*options = {is_async : false}*/){
+    //     const newTest = {
+    //         name : descripton,
+    //         actual : actual_output,
+    //         expected : expected_output
+    //     }
 
-        this.TESTS.push(newTest);
-    }
+    //     this.TESTS.push(newTest);
+    // }
 
-    run() { 
-        this.TESTS.forEach(test => {
-            // const {is_async} = test.options;
-            // if(is_async) {
-            //     this.run_async(test);
-            // }
-            // else {
-                const output = test.execute();
-                if(output === test.expected) 
-                    this.passed_test(test.name);
-                else 
-                    this.failed_test(test,output);
-            //}
-        });
+    // run_all() { 
+    //     this.TESTS.forEach((_,index) => run(index));
+    //     this.summary_tests();
+    //     this.reset_stats();
+    // }
 
-        this.summary_tests();
-        this.reset_stats();
-    }
-    async run_async(test){
-        try {
-            const output = await test.execute();
-            if(output === test.expected_output) this.passed_test(test.name);
-            else this.failed_test(test.name);
-        } catch(error){
-            this.failed_test(test.name);
-        }
-
+    /*
+     * Parameters:
+     * const test = {
+     *      name : 'Name/Description',
+     *      actual : 'Output from the function to be tested'
+     *      expected : 'Correct output to be matched'
+     * }
+     * e.g : run({name:'add 2 and 3',acutal: 5, expected: 5})
+     */
+    run(name,actual,expected) {
+        this.total_tests += 1;
+        
+        const actual_json = JSON.stringify(actual);
+        const expected_json = JSON.stringify(expected);
+        if(actual_json === expected_json) {
+            this.passed_test(name);
+            return true;
+        } 
+        
+        this.failed_test(name,actual,expected);
+        return false;
     }
 };
 
-module.exports = Testing;
-
-/*
-//testing the Testing class
-const test = new Testing();
+module.exports = { Testing, color };
 
 
-//create dummy function
-function addP(num1, num2) {
-    return num1 + num2;
-}
+// //testing the Testing class
+// const test = new Testing();
 
-function addF(num1, num2) {
-    return num1 - num2;
-}
 
-test.add('adding 1 and 2 addP',()=>{ return addP(1,2); },3);
-test.add('adding 1 and 2 addF',()=>{ return addF(1,2); },3);
+// //create dummy function
+// function addP(num1, num2) {
+//     return num1 + num2;
+// }
 
-test.run();
-*/
+// function addF(num1, num2) {
+//     return num1 - num2;
+// }
 
+// test.run('adding 1 and 2 addP',addP(1,2),3);
+// test.run('adding 1 and 2 addF',addF(1,2),3);
+
+// test.summary();
 
